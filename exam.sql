3bfee5308bdf9ffc38d0b7f923e7a93cbc470051
Urmat Bekboev <bek.urmat@gmail.com>
2019-03-30 16:41:26 +0600

    datetime length extended (bug fix)

--- exam.sql
+++ exam.sql
@@ -6,14 +6,7 @@ CREATE TABLE `news` (
   `title` VARCHAR(50) NOT NULL,
   `description` TEXT NOT NULL,
   `image` VARCHAR(100) NULL,
-  `datetime` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`id`));
-
-CREATE TABLE `comments` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `news_id` INT NULL,
-  `author` VARCHAR(45) NULL DEFAULT 'Anonymous',
-  `message` VARCHAR(255) NOT NULL,
+  `datetime` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`));
 
 CREATE TABLE `comments` (
@@ -32,14 +25,14 @@ CREATE TABLE `comments` (
 
 INSERT INTO `news` (`id`, `title`, `description`, `datetime`)
 VALUES
-    (1, 'Bishkek news', 'This is first place', 20190330),
-    (2, 'Naryn news', 'This is second place', 20190330),
-    (3, 'Karakol news', 'This is third place', 20190330),
-    (4, 'Osh news', 'This is fourth place', 20190330);
+    (1, 'Bishkek news', 'This is first place', '2018-02-23T10:26:00.996Z'),
+    (2, 'Naryn news', 'This is second place', '2018-03-23T10:26:00.996Z'),
+    (3, 'Karakol news', 'This is third place', '2018-01-23T10:26:00.996Z'),
+    (4, 'Osh news', 'This is fourth place', '2018-07-23T10:26:00.996Z');
 
 INSERT INTO `comments` (`id`, `news_id`, `author`, `message`)
 VALUES
     (1, 4, 'Alena Biburashvili', 'This is my first comment'),
     (2, 3, 'Nikita Vasilievich', 'This is my second comment'),
-    (3, 2, null, 'This is my fourth comment'),
+    (3, 2, 'Anonymous', 'This is my fourth comment'),
     (4, 1, 'PAVEL ON SAMYI', 'This is my fives comment');
